7271f4f7356e759a927d45969c36d42b58d6487b
Beth Schofield <bethmschofield@gmail.com>
2019-09-20 15:05:27 +0000

    printing good dogs

--- calendar.rb
+++ calendar.rb
@@ -1,8 +1,11 @@
+require_relative './dogs.rb'
+
 puts "Here is Masha's feeding cal!"
 puts "What would you like to see?"
 i = gets.strip
 if i == "Dogs!"
     puts "yay here are dogs!"
+    good_boys.each{|dog| puts dog}
 elsif i == "Cats!"
     puts "hmmm... I don't know about cats"
 else 
